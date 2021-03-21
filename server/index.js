1a37bcc8c34b524cb6e0aaffcf0e1cdb78f29ba3
mukhammed musa <muhammedmusaaa@gmail.com>
2021-03-21 16:01:04 +0800

    server was changed

--- server/index.js
+++ server/index.js
@@ -28,6 +28,7 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`The server is listening on port ${port}`));
 
+
 // app.get('/', (req, res) => {
 //     res.send('<h1>Hello World!</h1>');
 // });
